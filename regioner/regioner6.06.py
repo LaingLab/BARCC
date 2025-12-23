bad80dfacb9af754aa2f1ebdfe7eed090c6c023e
TopHat327 <123082352+TopHat327@users.noreply.github.com>
2025-12-23 13:29:20 -0600

    Add license and copyright information

--- regioner/regioner6.06.py
+++ regioner/regioner6.06.py
@@ -1,3 +1,20 @@
+#    BARC (Brain Atlas Regional Counter) is a software that performs automatic cell counting 
+#    of microscopy images and assists in the automation of image workup. 
+#    Copyright (C) <2025>  <George Taylor>
+
+#    This program is free software: you can redistribute it and/or modify
+#    it under the terms of the GNU Affero General Public License as published
+#    by the Free Software Foundation, either version 3 of the License, or
+#    (at your option) any later version.
+
+#    This program is distributed in the hope that it will be useful,
+#    but WITHOUT ANY WARRANTY; without even the implied warranty of
+#    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+#    GNU Affero General Public License for more details.
+
+#    You should have received a copy of the GNU Affero General Public License
+#    along with this program.  If not, see <https://www.gnu.org/licenses/>.
+
 #!/usr/bin/env python3
 import fitz
 import tkinter as tk
